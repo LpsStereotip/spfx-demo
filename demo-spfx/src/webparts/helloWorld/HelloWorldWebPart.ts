bb3ad54d5c701cff5aa2c5865f2eab580d0a0668
Vladymyr Tkachenko <alex.pashkevych@axleinfo.com>
2019-07-15 10:10:24 -0400

    Hello World wb part changes to get build files from demo project

--- demo-spfx/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ demo-spfx/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -9,7 +9,9 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import * as strings from 'HelloWorldWebPartStrings';
 
-import 'demo/dist/demo/bundle.js';
+import 'demo/dist/demo/app.ad0c573bd7106cee5456.css';
+import 'demo/dist/demo/polyfills.18888b24c630850bb236.js';
+import 'demo/dist/demo/app.2cd138e9b3ce8bbfc4ff.js';
 
 export interface IHelloWorldWebPartProps {
   description: string;
@@ -18,7 +20,7 @@ export interface IHelloWorldWebPartProps {
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
   public render(): void {
-    this.domElement.innerHTML = `<app-hello-world-web-part description="${ this.properties.description }"></app-hello-world-web-part>`;
+    this.domElement.innerHTML = `<app-root></app-root>`;
   }
 
   protected get dataVersion(): Version {
